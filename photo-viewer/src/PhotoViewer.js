8f2a60d2bad5bf70a837c2169424db94d13ab8fe
caramonica <caramonica@hotmail.co.uk>
2021-09-03 12:21:39 +0100

    main photo added

---
+++ photo-viewer/src/PhotoViewer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./PhotoViewer.css"
+
+export const PhotoViewer = () => (
+  <div>
+    <img className="main-image" src="https://picsum.photos/200/300" alt="random photo" />
+  </div>
+);
